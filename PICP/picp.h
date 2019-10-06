e8b473cbc1afafa2bf6e31ffe403b044185e76b4
CHHAVI SHARMA <chhavis@park.key.seas.upenn.edu>
2019-10-06 01:17:07 -0400

    Restarted implementation from Proj2 to untilise simulation-kernels

--- PICP/picp.h
+++ PICP/picp.h
@@ -2,8 +2,11 @@
 
 #include "common.h"
 
-namespace CharacterRecognition {
+namespace ParallelICP {
     Common::PerformanceTimer& timer();
 
     // TODO: implement required elements for MLP sections 1 and 2 here
+
+	void computeICPNaive(int Y, int X, float * Ybuffer, float * Xbuffer);
+
 }
